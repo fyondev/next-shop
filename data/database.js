7223cb5be83288a647426f7f2ca2fbd03a94ca51
Santiago Laight <santilaight@santiagos-macbook-pro.home>
2021-07-15 12:36:58 +0200

    adding admin page

--- data/database.js
+++ data/database.js
@@ -5,4 +5,10 @@ db.version(1).stores({
   cartItems: `name, price, description, image, count`,
 });
 
+
+//adding to open DB on import + error message for DB
+db.open().catch((err)=>{
+  console.error('Database could not be opened: '+ (err.stack || err));
+})
+
 export default db;
