bf4e515863615fb9ec686c93fce590bf9d3e69bb
hridi <parizatbintekabir@gmail.com>
2021-07-16 17:28:52 +0600

    Added sorting by price button

--- components/product/list.js
+++ components/product/list.js
@@ -1,15 +1,45 @@
 import { SingleProductComponent } from "./single";
-
-export const ListProductComponent = ({ products = [], handleAddToCartClick }) => {
+import React, { useState } from "react";
+export const ListProductComponent = ({
+  products = [],
+  handleAddToCartClick,
+}) => {
+  const [sortProduct, setSortProduct] = useState(false);
+  const sortByPrice = () => {
+    setSortProduct(!sortProduct);
+  };
+  const compare = (a, b) => {
+    return a.price - b.price;
+  };
+  const sortedProductByPrice = () => {
+    return products.sort(compare);
+  };
+  console.log(products);
   return (
-    <div className="flex">
-      {products.map((product) => (
-        <SingleProductComponent
-          key={product.name}
-          product={product}
-          onAddToCartClick={handleAddToCartClick}
-        />
-      ))}
-    </div>
+    <>
+      <button
+        className="px-3 py-2 bg-indigo-200 text-gray-800"
+        onClick={sortByPrice}
+      >
+        Sort By
+      </button>
+      <div className="flex">
+        {!!sortProduct
+          ? sortedProductByPrice().map((product) => (
+              <SingleProductComponent
+                key={product.name}
+                product={product}
+                onAddToCartClick={handleAddToCartClick}
+              />
+            ))
+          : products.map((product) => (
+              <SingleProductComponent
+                key={product.name}
+                product={product}
+                onAddToCartClick={handleAddToCartClick}
+              />
+            ))}
+      </div>
+    </>
   );
 };
